c30cce641eb1096ba99b993df57b7ecacf569cce
AlexMuir <me@alexmuir.com>
2015-06-26 09:41:45 +0200

    Annotates the story

--- test/models/news/story_test.rb
+++ test/models/news/story_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: news_stories
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  url        :string
+#  image      :string
+#  points     :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 module News

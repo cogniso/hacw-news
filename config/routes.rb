dce63dc718adee6c26474e4e77e0ac6c737671e1
fardin <fardin_k2004@yahoo.com>
2015-07-09 21:22:52 +0200

    Vote arrows updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ News::Engine.routes.draw do
   root to: 'stories#index'
   get 'seed'=>'stories#seed'
   get 'submit'=>'stories#new'
+  get 'new' => 'stories#new'
   resources :stories do
     get :vote, on: :member
   end

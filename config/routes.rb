7b11978a8d248699885ffa15fe3b7ebcb9faead9
fardin <fardin_k2004@yahoo.com>
2015-07-09 14:22:15 +0200

    Freddies changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 News::Engine.routes.draw do
   root to: 'stories#index'
-  get 'seed'=>'stories#seed'
-  get 'submit'=>'stories#new'
+  get '/seed'=>'stories#seed'
+  get '/submit'=>'stories#new'
   resources :stories
 end
